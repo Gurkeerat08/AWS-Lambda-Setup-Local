ab5c0b82139bd4ac131cd5852c3c1acf02ecd998
Gurkeerat Singh Sondhi <152252215+Gurkeerat08@users.noreply.github.com>
2024-11-21 18:11:03 +0530

    added code to generate a json file

--- environment_variables.py
+++ environment_variables.py
@@ -1,23 +1,30 @@
 import boto3
+import json
 
 try: 
     # Set up your AWS credentials
     session = boto3.Session(
-        aws_access_key_id='YOUR_KEY',
-        aws_secret_access_key='YOUR_KEY',
-        aws_session_token='YOUR_KEY'
+        aws_access_key_id = "actual-value-here",
+        aws_secret_access_key = "actual-value-here",
+        aws_session_token = "actual-value-here"
     )
 
     # Create a Lambda client
     lambda_client = session.client('lambda',region_name='us-east-2')
-
-    # Specify the Lambda Name
-    # For example , FunctionName='aads-marketplace-get-data-request-history-dev'
-    response = lambda_client.get_function_configuration(FunctionName='aads-marketplace-get-data-request-history-dev')
+    lambda_name = 'lambda-name-here'
+    # Get the function configuration
+    response = lambda_client.get_function_configuration(FunctionName=lambda_name)
 
     # Extract environment variables
     env_variables = response['Environment']['Variables']
-    print(env_variables)
+    # Convert the dictionary to a JSON string
+    json_data = json.dumps(env_variables, indent=4)
+
+    # Write the JSON string to a file
+    with open(lambda_name+'env_variables.json', 'w') as json_file:
+        json_file.write(json_data)
+
+    print("JSON file created successfully for the lambda : "+lambda_name)
 except Exception as e:
     # Code that runs if an exception occurs
     print(f"An error occurred: {e}")
